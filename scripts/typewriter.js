9ed9e82401c2132e5ad25910d5b2a55da5cb3bb6
spooketti <jonliu168@gmail.com>
2023-09-08 20:21:38 -0700

    konami code universal

--- scripts/typewriter.js
+++ scripts/typewriter.js
@@ -3,3 +3,39 @@
         console.log("bruh")
         header.style.width = `${header.innerText.length}ch`
         })
+
+        let sequence = [38,38,40,40,37,39,37,39,66,65]
+    let pos = 0;
+document.addEventListener('keydown', function(e) 
+  {
+  let expected = sequence[pos]
+  if(e.keyCode == expected)
+  {
+    pos++;
+    if(pos == 10)
+    {
+      var am,bm,cm
+      function fun() 
+      {
+         
+am="-webkit",bm='transform:rotate(1turn);',cm='transition:4s;';
+        document.body.style += am+bm+am+cm+bm+cm
+      }
+    
+
+  fun();
+      setTimeout(() => {
+   document.body.style -= am+bm+am+cm+bm+cm
+}, "4000") 
+      
+      
+        pos = 0;
+      
+        }
+      
+  }
+else
+  {
+    pos = 0;
+  }
+})
